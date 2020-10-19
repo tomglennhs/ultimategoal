479c380fe510c16c73e41a867ff6986d1e3512d2
Jason Antwi-Appah <jasonantwiappah@gmail.com>
2020-10-18 23:10:52 -0500

    Added a boilerplate opmode to use as a template (untested but should work)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TemplateOpMode.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+// delete this next line once you've made a copy of the class
+@Disabled
+@TeleOp(name = "TemplateOpMode", group = "Does Literally Nothing")
+public class TemplateOpMode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Place your init code here. Initialize motors and sensors here
+        MotorEx motor = new MotorEx(hardwareMap, "motor");
+
+        // keep this here, this indicates the end of the init
+        waitForStart();
+
+        /*
+            Insert your driving code after this comment.
+            Keep in mind this is a linear OpMode, so anything you put here will run once.
+            If you want it to loop, you'll have to put into the while block.
+            Anything in the while loop will run until the driver hits stop.
+        */
+
+        while (opModeIsActive()) {
+            telemetry.addData("Hmm?", "Yes");
+            telemetry.update();
+        }
+
+    }
+}
