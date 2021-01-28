5ced699e6964705d94951b73a7de867e81706a7f
Jason Antwi-Appah <hey@jasonaa.me>
2021-01-28 22:00:01 +0000

    fix: remove needs key from release steps

---
+++ scripts/create_release.js
@@ -0,0 +1,3 @@
+// decide whether to create a major or minor release by checking if the SDK version has changed in ultimategoal/FtcRobotController/src/main/AndroidManifest.xml
+// run `release <major|minor>`
+// continue with life ig
