9439ccaa2eead5a05ffe5533c69158a0c30272c9
Jason Antwi-Appah <hey@jasonaa.me>
2021-02-24 14:16:03 +0000

    feat(shooter): use deg/s as velocity units

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterSys.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ShooterSys.java
@@ -4,33 +4,37 @@ import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
 
 public class ShooterSys extends SubsystemBase {
     private final MotorEx motor;
     private boolean isRunning;
-    private final double runVelocity = 24.0;
+    // eventually i'll want to make these tunable by ftc dashboard
+    private final boolean invert = false;
+    // velocity is in degrees per second. the theoretical max is 36_000 deg/s since the motor has a max rpm of 6000
+    private final double runVelocity = 34_000.0;
     private final double stopVelocity = 0.0;
-    private final double velocityTolerance = 0.5;
+    private final double velocityTolerance = 50.0;
     private final double maxVelocityTolerance = runVelocity + velocityTolerance;
     private final double minVelocityTolerance = runVelocity - velocityTolerance;
+
 
     public ShooterSys(final HardwareMap hMap) {
         motor = new MotorEx(hMap, "shooter");
         motor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.FLOAT);
         motor.setRunMode(Motor.RunMode.VelocityControl);
-        motor.setInverted(false);
+        motor.setInverted(invert);
         isRunning = false;
     }
 
     public void start() {
-        // i believe setVelocity units are counts per revolution and the max for ultraplanetary motors is 28. we'll see if im wrong when we test. 
-        // if that is wrong, the units are probably ticks per second
-        motor.setVelocity(runVelocity);
+        motor.setVelocity(runVelocity, AngleUnit.DEGREES);
         isRunning = true;
     }
 
     public void stop() {
-        motor.setVelocity(stopVelocity);
+        motor.setVelocity(stopVelocity, AngleUnit.DEGREES);
         isRunning = false;
     }
 
